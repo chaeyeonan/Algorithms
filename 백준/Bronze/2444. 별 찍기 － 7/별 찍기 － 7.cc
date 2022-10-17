80cb6275f986c62f41cc334449c390ecbe0c04ad
Chaeyeon An <90609254+chaeyeonan@users.noreply.github.com>
2022-10-17 23:53:08 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    
+    for(int i=0; i<N; i++){
+        
+         //공백 출력
+        for(int j=N-1; j>i; j--){
+            cout<<" ";
+        }
+        
+        //별 출력
+        for(int m=1; m<=2*(i+1)-1; m++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+    for(int i=1;i<N;i++){
+         
+        //공백 출력
+        for(int j=1; j<=i; j++){
+            cout<<" ";
+        }
+        
+        //별 출력
+        for(int m=2*(N-i)-1; m>=1; m--){
+            cout<<"*";
+        }
+        
+        cout<<"\n";
+    }
+    return 0;
+}
