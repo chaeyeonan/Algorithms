17cdede16e362f5148bbcc5ffc8bfc34703ec7c2
Chaeyeon An <90609254+chaeyeonan@users.noreply.github.com>
2022-10-23 16:21:23 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -2,30 +2,25 @@
 
 using namespace std;
 
-int arr[100],pos[100],neg[100];
+int pos[100],neg[100];
 
 int main()
 {
-    int n,x;
+    int n,t,x;
     
-    //입력
     cin>>n;
     for (int i=0; i<n; i++){
-        cin>>arr[i];
+        cin>>t;
+        
+        if(t>=0){
+            pos[t]++;
+        }
+        else if(t<0){
+            neg[-t]++;
+        }
     }
     cin>>x;
     
-    // 입력하는 숫자가 양수, 음수에 따라 다른 배열에 저장
-    for (int i=0; i<n; i++){
-        if(arr[i]>=0){
-            pos[arr[i]]++;
-        }
-        else if(arr[i]<0){
-            neg[-arr[i]]++;
-        }
-    }
-    
-    // 찾는 숫자가 양수, 음수에 따라 다른 배열에서 개수 찾음
     if(x>=0) cout<<pos[x];
     else if(x<0) cout<<neg[-x];
     
