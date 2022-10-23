cfb3d3d6479cb77053d486b72a65a3dab4480f53
Chaeyeon An <90609254+chaeyeonan@users.noreply.github.com>
2022-10-24 00:06:28 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int room = 0;
+
+int roomNum(int i, int k) {
+
+	if (i % k == 0) { //i가 짝수
+		room += i / k;
+	}
+	else {
+		room += i / k + 1;
+	}
+
+	return room;
+}
+
+int main() {
+
+	int n, k, s, g;
+	int boy[7] = {}, girl[7] = {};
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> s >> g;
+
+		if (s == 0) girl[g]++;
+		else boy[g]++;
+	}
+
+	for (int i = 1; i < 7; i++) {
+		if (girl[i] > 0) roomNum(girl[i], k);
+	}
+
+	for (int i = 1; i < 7; i++) {
+		if (boy[i] > 0) roomNum(boy[i], k);
+	}
+
+	cout << room;
+
+	return 0;
+
+}
